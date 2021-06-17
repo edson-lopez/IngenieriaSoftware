bcb3110334a67861787e9feb974fca768464c83d
elopez96 <elopez@guaitech.com>
2021-06-17 11:48:45 +0200

    removed frms

--- Views/Views/Main.Designer.cs
+++ Views/Views/Main.Designer.cs
@@ -29,75 +29,30 @@ namespace Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnNuevaOrden = new System.Windows.Forms.Button();
-            this.x1 = new System.Windows.Forms.GroupBox();
-            this.btnOrdenes = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnClientes = new System.Windows.Forms.Button();
             this.btnNuevoCliente = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnDepartamentos = new System.Windows.Forms.Button();
             this.btnEstadoFabrica = new System.Windows.Forms.Button();
-            this.x1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnNuevaOrden
-            // 
-            this.btnNuevaOrden.Location = new System.Drawing.Point(103, 44);
-            this.btnNuevaOrden.Name = "btnNuevaOrden";
-            this.btnNuevaOrden.Size = new System.Drawing.Size(271, 60);
-            this.btnNuevaOrden.TabIndex = 0;
-            this.btnNuevaOrden.Text = "Nueva Orden";
-            this.btnNuevaOrden.UseVisualStyleBackColor = true;
-            this.btnNuevaOrden.Click += new System.EventHandler(this.btnNuevaOrden_Click);
-            // 
-            // x1
-            // 
-            this.x1.Controls.Add(this.btnOrdenes);
-            this.x1.Controls.Add(this.btnNuevaOrden);
-            this.x1.Location = new System.Drawing.Point(741, 71);
-            this.x1.Name = "x1";
-            this.x1.Size = new System.Drawing.Size(424, 224);
-            this.x1.TabIndex = 1;
-            this.x1.TabStop = false;
-            this.x1.Text = "Vehiculos";
-            // 
-            // btnOrdenes
-            // 
-            this.btnOrdenes.Location = new System.Drawing.Point(103, 127);
-            this.btnOrdenes.Name = "btnOrdenes";
-            this.btnOrdenes.Size = new System.Drawing.Size(271, 60);
-            this.btnOrdenes.TabIndex = 1;
-            this.btnOrdenes.Text = "Historial";
-            this.btnOrdenes.UseVisualStyleBackColor = true;
-            this.btnOrdenes.Click += new System.EventHandler(this.btnOrdenes_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnClientes);
             this.groupBox1.Controls.Add(this.btnNuevoCliente);
-            this.groupBox1.Location = new System.Drawing.Point(741, 301);
+            this.groupBox1.Location = new System.Drawing.Point(359, 102);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(424, 224);
+            this.groupBox1.Size = new System.Drawing.Size(424, 138);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Clientes";
             // 
-            // btnClientes
-            // 
-            this.btnClientes.Location = new System.Drawing.Point(103, 127);
-            this.btnClientes.Name = "btnClientes";
-            this.btnClientes.Size = new System.Drawing.Size(271, 60);
-            this.btnClientes.TabIndex = 1;
-            this.btnClientes.Text = "Notificaciones";
-            this.btnClientes.UseVisualStyleBackColor = true;
-            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
-            // 
             // btnNuevoCliente
             // 
-            this.btnNuevoCliente.Location = new System.Drawing.Point(103, 44);
+            this.btnNuevoCliente.Location = new System.Drawing.Point(74, 51);
             this.btnNuevoCliente.Name = "btnNuevoCliente";
             this.btnNuevoCliente.Size = new System.Drawing.Size(271, 60);
             this.btnNuevoCliente.TabIndex = 0;
@@ -107,28 +62,17 @@ namespace Views
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.btnDepartamentos);
             this.groupBox2.Controls.Add(this.btnEstadoFabrica);
-            this.groupBox2.Location = new System.Drawing.Point(741, 531);
+            this.groupBox2.Location = new System.Drawing.Point(359, 281);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(424, 224);
+            this.groupBox2.Size = new System.Drawing.Size(424, 138);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Fábrica";
             // 
-            // btnDepartamentos
-            // 
-            this.btnDepartamentos.Location = new System.Drawing.Point(103, 127);
-            this.btnDepartamentos.Name = "btnDepartamentos";
-            this.btnDepartamentos.Size = new System.Drawing.Size(271, 60);
-            this.btnDepartamentos.TabIndex = 1;
-            this.btnDepartamentos.Text = "Departamentos";
-            this.btnDepartamentos.UseVisualStyleBackColor = true;
-            this.btnDepartamentos.Click += new System.EventHandler(this.btnDepartamentos_Click);
-            // 
             // btnEstadoFabrica
             // 
-            this.btnEstadoFabrica.Location = new System.Drawing.Point(103, 44);
+            this.btnEstadoFabrica.Location = new System.Drawing.Point(74, 52);
             this.btnEstadoFabrica.Name = "btnEstadoFabrica";
             this.btnEstadoFabrica.Size = new System.Drawing.Size(271, 60);
             this.btnEstadoFabrica.TabIndex = 0;
@@ -136,35 +80,41 @@ namespace Views
             this.btnEstadoFabrica.UseVisualStyleBackColor = true;
             this.btnEstadoFabrica.Click += new System.EventHandler(this.btnEstadoFabrica_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 200);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1256, 771);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(842, 549);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.x1);
             this.Name = "Main";
             this.Text = "Automóviles Uneatlantico";
             this.Load += new System.EventHandler(this.Main_Load);
-            this.x1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnNuevaOrden;
-        private System.Windows.Forms.GroupBox x1;
-        private System.Windows.Forms.Button btnOrdenes;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnClientes;
         private System.Windows.Forms.Button btnNuevoCliente;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button btnDepartamentos;
         private System.Windows.Forms.Button btnEstadoFabrica;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
